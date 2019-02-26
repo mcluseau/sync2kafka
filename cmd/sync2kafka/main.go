7dcc37362309518156717294b34dec210485c467
Mikaël Cluseau <mcluseau@isi.nc>
2019-02-26 15:50:03 +1100

    feat: TCP keepalive setup

--- cmd/sync2kafka/main.go
+++ cmd/sync2kafka/main.go
@@ -5,12 +5,14 @@ import (
 	"flag"
 	"log"
 	"net"
+	"time"
 )
 
 var (
-	tlsKeyPath  = flag.String("tls-key", "", "TLS key path (listen with TLS encryption if set)")
-	tlsCertPath = flag.String("tls-cert", "", "TLS certificate path (required if key is set)")
-	bindSpec    = flag.String("bind", ":9084", "Listen specification (host:port)")
+	tlsKeyPath      = flag.String("tls-key", "", "TLS key path (listen with TLS encryption if set)")
+	tlsCertPath     = flag.String("tls-cert", "", "TLS certificate path (required if key is set)")
+	bindSpec        = flag.String("bind", ":9084", "Listen specification (host:port)")
+	keepAlivePeriod = flag.Duration("tcp-keepalive-period", 30*time.Second, "TCP keepalive period")
 )
 
 func main() {
@@ -27,15 +29,25 @@ func main() {
 		go indexTopic(*targetTopic)
 	}
 
-	var listener net.Listener
+	var tlsConfig *tls.Config
+	tlsMode := len(*tlsKeyPath) != 0
+	if tlsMode { // TLS mode, prepare tlsConfig
+		cert, err := tls.LoadX509KeyPair(*tlsCertPath, *tlsKeyPath)
+		if err != nil {
+			log.Fatal("failed to load TLS key pair: ", err)
+		}
 
-	if len(*tlsKeyPath) != 0 { // TLS mode
-		listener = tlsListener()
-		log.Print("TLS listening on ", *bindSpec)
-	} else {
-		listener = standardListener()
-		log.Print("listening on ", *bindSpec)
+		tlsConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
 	}
+
+	listener, err := net.Listen("tcp", *bindSpec)
+	if err != nil {
+		log.Fatalf("failed to listen on %s: %v", *bindSpec, err)
+	}
+
+	log.Printf("listening on %s (TLS: %v)", *bindSpec, tlsMode)
 
 	for {
 		conn, err := listener.Accept()
@@ -43,33 +55,19 @@ func main() {
 			log.Fatal("listener failed: ", err)
 		}
 
+		switch c := conn.(type) {
+		case *net.TCPConn:
+			c.SetKeepAlivePeriod(*keepAlivePeriod)
+			c.SetKeepAlive(true)
+
+		default: // should not happen
+			log.Print("connection is not TCP?!")
+		}
+
+		if tlsMode {
+			conn = tls.Server(conn, tlsConfig)
+		}
+
 		go handleConn(conn)
 	}
-}
-
-func standardListener() net.Listener {
-	l, err := net.Listen("tcp", *bindSpec)
-
-	if err != nil {
-		log.Fatalf("failed to listen on %s: %v", *bindSpec, err)
-	}
-
-	return l
-}
-
-func tlsListener() net.Listener {
-	cert, err := tls.LoadX509KeyPair(*tlsCertPath, *tlsKeyPath)
-	if err != nil {
-		log.Fatal("failed to load TLS key pair: ", err)
-	}
-
-	l, err := tls.Listen("tcp", *bindSpec, &tls.Config{
-		Certificates: []tls.Certificate{cert},
-	})
-
-	if err != nil {
-		log.Fatalf("TLS failed to listen on %s: %v", *bindSpec, err)
-	}
-
-	return l
 }
